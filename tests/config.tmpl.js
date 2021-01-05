4e516feada8f765c271333db04ff086ee8fc8a65
fangqk1991 <me@fangqk.com>
2021-01-05 12:06:53 +0800

    v0.0.1: ali-oss-download 初版

---
+++ tests/config.tmpl.js
@@ -0,0 +1,9 @@
+module.exports = {
+  visitor: {
+    region: '<OSS region>',
+    accessKeyId: '<OSS accessKeyId>',
+    accessKeySecret: '<OSS accessKeySecret>',
+    bucket: '<OSS bucket name>',
+    secure: true,
+  },
+}
